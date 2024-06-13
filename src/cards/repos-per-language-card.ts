5d69cfb191320c6de0d0a2a638562ff46b8e3477
zetaloop <zetaloop@outlook.com>
2024-06-13 11:06:30 +0800

    Test custom theme

--- src/cards/repos-per-language-card.ts
+++ src/cards/repos-per-language-card.ts
@@ -1,4 +1,4 @@
-import {ThemeMap} from '../const/theme';
+import {ThemeMap, Theme} from '../const/theme';
 import {getRepoLanguages} from '../github-api/repos-per-language';
 import {createDonutChartCard} from '../templates/donut-chart-card';
 import {writeSVG} from '../utils/file-writer';
@@ -6,7 +6,7 @@ import {writeSVG} from '../utils/file-writer';
 export const createReposPerLanguageCard = async function (username: string, exclude: Array<string>) {
     const langData = await getRepoLanguageData(username, exclude);
     for (const themeName of ThemeMap.keys()) {
-        const svgString = getReposPerLanguageSVG(langData, themeName);
+        const svgString = getReposPerLanguageSVG(langData, themeName, undefined);
         // output to folder, use 1- prefix for sort in preview
         writeSVG(themeName, '1-repos-per-language', svgString);
     }
@@ -15,22 +15,27 @@ export const createReposPerLanguageCard = async function (username: string, excl
 export const getReposPerLanguageSVGWithThemeName = async function (
     username: string,
     themeName: string,
-    exclude: Array<string>,
-    noBg: boolean,
-    noStroke: boolean
+    customTheme: Theme,
+    exclude: Array<string>
 ) {
     if (!ThemeMap.has(themeName)) throw new Error('Theme does not exist');
     const langData = await getRepoLanguageData(username, exclude);
-    return getReposPerLanguageSVG(langData, themeName, noBg, noStroke);
+    return getReposPerLanguageSVG(langData, themeName, customTheme);
 };
 
-const getReposPerLanguageSVG = function (langData: { name: string; value: number; color: string }[], themeName: string, noBg = false, noStroke = false) {
-    var theme = ThemeMap.get(themeName)!;
-    if (noBg) {
-        theme.background = 'transparent';
-    }
-    if (noStroke) {
-        theme.strokeOpacity = 0;
+const getReposPerLanguageSVG = function (
+    langData: { name: string; value: number; color: string }[],
+    themeName: string,
+    customTheme: Theme | undefined
+) {
+    let theme = { ...ThemeMap.get(themeName)! };
+    if (customTheme !== undefined) {
+        if (customTheme.title) theme.title = customTheme.title;
+        if (customTheme.text) theme.text = customTheme.text;
+        if (customTheme.background) theme.background = customTheme.background;
+        if (customTheme.stroke) { theme.stroke = customTheme.stroke; theme.strokeOpacity = 1; }
+        if (customTheme.icon) theme.icon = customTheme.icon;
+        if (customTheme.chart) theme.chart = customTheme.chart;
     }
     const svgString = createDonutChartCard('Top Languages by Repo', langData, theme);
     return svgString;
